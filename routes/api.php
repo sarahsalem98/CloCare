bbbc76030c7d19bf547c524e2eca9267631866cf
sarahsalem98 <sasasalem44@gmail.com>
2021-04-17 00:28:13 +0200

    edited api

--- routes/api.php
+++ routes/api.php
@@ -18,13 +18,16 @@ use Illuminate\Support\Facades\App;
 
 
 Route::middleware('auth:sanctum')->group(function(){
-  Route::get('/user', function (Request $request) {
-        return $request->user();
+  // Route::get('/user', function (Request $request) {
+  //   return "advlm";
        
-    });
+  //   });
 
 });
-
+Route::get('/user', function (Request $request) {
+  return "advlm";
+     
+  });
 Route::middleware('auth:doctor')->group(function(){
 
   Route::post('/addreports/{id}','App\Http\Controllers\Api\Doctors\DoctorController@AddReports');
